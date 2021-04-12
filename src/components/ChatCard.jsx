de50ae9f5521b4492a527be0070b9568ced4353a
Hasin, Michael <michael.hasin@intel.com>
2021-04-12 16:14:12 +0300

    chat card component

---
+++ src/components/ChatCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import userPlaceHolder from "../assets/userPlaceHolder.png";
+
+export default function ChatCard({ image, name, lastSeen, onClick, active }) {
+  let classes = "user-card " + (active ? "active" : "");
+  return (
+    <div className={classes} onClick={onClick}>
+      <img src={image ?? userPlaceHolder} alt="user" />
+      <div>{name}</div>
+      <div>{new Date(lastSeen).toLocaleDateString("en-US")}</div>
+    </div>
+  );
+}
