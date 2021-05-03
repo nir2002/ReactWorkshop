2f931c5625caaa0fd826e6d03efe523bff769e83
Nir Parisian <nir2002@users.noreply.github.com>
2021-05-03 23:24:46 +0300

    supporting basic messaging

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,3 +1,22 @@
-export default function ChatMessages() {
-  return <div className="chat-messages">Chat window</div>;
+export default function ChatMessages({ messages }) {
+  return (
+    <div className="chat-messages">
+      {messages &&
+        messages.map((message) => (
+          <Message from={message.user} time={message.time}>
+            {message.content}
+          </Message>
+        ))}
+    </div>
+  );
+}
+
+function Message({ from, time, children }) {
+  return (
+    <div class="message-container">
+      <div class="message-from">{from}</div>
+      <div class="message-content">{children}</div>
+      <div class="message-time">{time}</div>
+    </div>
+  );
 }
